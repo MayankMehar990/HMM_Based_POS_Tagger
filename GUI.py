afe375979c770434ef421992c8b60eb94ff8d506
MayankMehar <132464056+MayankMehar990@users.noreply.github.com>
2025-05-26 14:49:50 +0530

    Update GUI to include crf_tagger.py

--- GUI.py
+++ GUI.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from HMM_pos_tagging import hmm_model
-
+from CRF_pos_Tagging import crf_model
 
 root = Tk()
 root.title("POS Tagger")
-root.geometry("560x350")
+root.geometry("560x400")
 
 
 label1 = Label(root, text="Enter a sentence : ",width=15,font=("Times New Roman",20))
@@ -19,29 +19,46 @@ label2.grid(row=1, column=0)
 output_box = Text(root,width=30,height=8,state='disabled')
 output_box.grid(row=1,column=1)
 
-def POS_Tagging(sentence):
+def hmm_Tagging(sentence):
     sentence=sentence.split()
     tagged_sentence=hmm_model.viterbi(sentence)
-    return " ".join(f"{word}/{tag} " for word,tag in tagged_sentence)
+    return " ".join(f"{word}/{tag} " for word, tag in tagged_sentence)
 
-def clicked():
+
+def crf_Tagging(sentence):
+    sentence=sentence.split()
+    tagged_sentence=crf_model.predict(sentence)
+    return " ".join(f"{word}/{tag} " for word, tag in zip(sentence,tagged_sentence))
+
+def hmm_clicked():
     label1.configure(text="Entered Sentence : ")
     sentence=entry1.get("1.0",END).strip()
-    tagged_sentence=POS_Tagging(sentence)
+    tagged_sentence=hmm_Tagging(sentence)
+    output_box.config(state='normal')  
+    output_box.delete("1.0", END) 
+    output_box.insert("1.0",tagged_sentence)     
+    output_box.config(state='disabled')
+
+def crf_clicked():
+    label1.configure(text="Entered Sentence : ")
+    sentence=entry1.get("1.0",END).strip()
+    tagged_sentence=crf_Tagging(sentence)
     output_box.config(state='normal')  
     output_box.delete("1.0", END) 
     output_box.insert("1.0",tagged_sentence)     
     output_box.config(state='disabled')
     
 
-button = Button(root, text="Tag",command=clicked,width=10,font=("Times New Roman",15))
-button.grid(row=0,column=1,pady=40)
-button.place(x=250,y=290)
+button1 = Button(root, text="HMM_Tagger",command=hmm_clicked,width=10,font=("Times New Roman",15))
+button1.grid(row=0,column=1,pady=40)
+button1.place(x=250,y=290)
+
+button1 = Button(root, text="CRF_Tagger",command=crf_clicked,width=10,font=("Times New Roman",15))
+button1.grid(row=0,column=1,pady=40)
+button1.place(x=400,y=290)
 
 close=Button(root, text="Close", command=root.destroy,width=10,font=("Times New Roman",15))
 close.grid(row=0,column=2,pady=40)
-close.place(x=400,y=290)
-
-
+close.place(x=320,y=340)
 
 root.mainloop()
